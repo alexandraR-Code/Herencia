d765c33870cd277736500a09fa46fd6449ca9009
Alexandra Ramirez <133824521+alexandraR-Code@users.noreply.github.com>
2026-09-23 17:42:05 -0500

    Clase objeto del metodo toString

--- src/com/krakedev/herencia/Test/TestHija.java
+++ src/com/krakedev/herencia/Test/TestHija.java
@@ -1,9 +1,18 @@
 package com.krakedev.herencia.Test;
 
+import com.krakedev.herencia.Hija;
+
 public class TestHija {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
+		
+		Hija hija = new Hija();
+		
+		hija.escucharBanBunny();
+		
+		
+		
 
 	}
 
---
+++ src/com/krakedev/herencia/Test/TestToString.java
@@ -0,0 +1,19 @@
+package com.krakedev.herencia.Test;
+
+import com.krakedev.herencia.Padre;
+
+public class TestToString {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		Padre p = new Padre();
+		
+		System.out.println(p);
+		
+		p.toString();
+		System.out.println(p.toString());
+
+	}
+
+}
